625b02d3613ff97165459e6e1cc5ce1fe6cee5e4
Arman Jasuja <arman_jasuja@yahoo.com>
2023-11-22 10:54:43 +0400

    Removing legacy code

--- web_app/app.py
+++ web_app/app.py
@@ -70,14 +70,7 @@ def index():
 @app.route("/dataset", methods=['GET', 'POST'])
 def dataset():        
     global training_complete
-    # global X_train
-    # global y_train
-    # global X_test
-    # global y_test
-    # global names
     global inputData
-    # global select_X_train
-    # global selection_model
     global large_value
 
     # Global variables
@@ -98,6 +91,7 @@ def dataset():
     global response_categories
     global prediction_category
 
+    # Debug statements- checking that we're transitioning between pages
     print(training_complete)
     print(request.form)
     if request.method == 'GET':
@@ -123,82 +117,8 @@ def dataset():
             for j in range(number_of_features):
                 min_feature_value[j] = np.min(feature_data[:, j])
                 max_feature_value[j] = np.max(feature_data[:, j])
-            # Make predictions and append them to the pandas dataframe and store it in a new file
-            # X = fraud_data.iloc[:, 0:47].to_numpy()
-            # y = fraud_data["PotentialFraud"].apply(lambda val: 0 if val == "Not-Fraud" else 1).to_numpy()
-            # # Split into train and test
-            # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
-            # Update user that feature selection has started
             training_complete = 2
             return redirect("/dataset", code=307)
-        # Feature select with XGBoost
-        # if training_complete == 2:
-        #     model = XGBClassifier()
-        #     model.fit(X_train, y_train)
-        #     y_pred = model.predict(X_test)
-        #     predictions = [round(value) for value in y_pred]    
-        #     accuracy = accuracy_score(y_test, predictions)
-        #     print("Accuracy: %.2f%%" % (accuracy * 100.0))
-        #     thresholds = sort(model.feature_importances_)
-        #     max_acc = -1
-        #     true_thresh = -1
-        #     for thresh in thresholds:
-        #         selection = SelectFromModel(model, threshold=thresh, prefit=True)
-        #         select_X_train = selection.transform(X_train)
-                
-        #         selection_model = XGBClassifier()
-        #         selection_model.fit(select_X_train, y_train)
-                
-        #         select_X_test = selection.transform(X_test)
-        #         y_pred = selection_model.predict(select_X_test)
-                
-        #         predictions = [round(value) for value in y_pred]
-                
-        #         accuracy = accuracy_score(y_test, predictions)
-        #         if accuracy >= max_acc and select_X_train.shape[1] < 20:
-        #             max_acc = accuracy
-        #             true_thresh = thresh
-        #         print("Thresh=%.3f, n=%d, Accuracy: %.2f%%" % (thresh, select_X_train.shape[1], accuracy*100.0))
-        #     selection = SelectFromModel(model, threshold=true_thresh, prefit=True)
-        #     select_X_train = selection.transform(X_train)
-
-            # Initialize model
-            # OCT parameters
-            # max_depth = 3
-            # min_samples_leaf = 10
-            # alpha = 0.00005
-            # time_limit = 10 # minute
-            # mip_gap_tol = 0.01  # optimal gap percentage
-            # mip_focus = 'balance'
-            # mip_polish_time = None
-            # warm_start = False
-            # log_file = None
-            # fp_heur = True
-            # backtrack = "bestb"
-            
-            # # Construct BNP-OCT classifier
-            # selection_model = BinNodePenaltyOptimalTreeClassifier(max_depth=max_depth,min_samples_leaf=min_samples_leaf,
-            #                                 alpha=alpha,
-            #                                 criterion="gini",
-            #                                 solver="gurobi",
-            #                                 time_limit=time_limit,
-            #                                 verbose=True,
-            #                                 warm_start=warm_start,
-            #                                 log_file=log_file,
-            #                                 solver_options={'mip_cuts': 'auto',
-            #                                                 'mip_gap_tol': mip_gap_tol,
-            #                                                 'mip_focus': mip_focus,
-            #                                                 'mip_polish_time': mip_polish_time,
-            #                                                 }
-            #                                 )
-            
-            # # Feature names
-            # for i in range(len(model.feature_importances_)):
-            #     if model.feature_importances_[i] >= true_thresh:
-            #         names.append(fraud_data.iloc[:, [i]].columns[0])
-            # print(len(names))
-            # training_complete = 3
-            # return redirect("/dataset", code=307)
         else:
             
             # Train model
@@ -210,49 +130,10 @@ def dataset():
             end = time.time()
             print(f"Running time: {end - start}")
             dot_data = array_to_dot(eng_vec, inputData)
-            # print(dot_data)
-            with open("tree.dot", "w") as f:
-                f.write(dot_data)
-            # s = graphviz.Source(dot_data, format="png")
-            # s.render('tree')
             graph = graphviz.Source(dot_data)
             graph.format = 'png'
             graph.render(filename='tree', directory='static/img/', view=False)
             print(eng_vec)
-            # selection_model.fit(select_X_train, y_train)
-            # # Make prediction
-            # # selection = SelectFromModel(model, threshold=true_thresh, prefit=True)
-            # # select_X_test = selection.transform(X_test)
-            # y_pred = selection_model.predict(X=select_X_train)
-            # y_pred_prob = selection_model.predict_proba(X=select_X_train)
-            
-            # # Check confusion matrix
-            # print("Confusion Matrix :")
-            # print(confusion_matrix(y_true=y_train,
-            #                     y_pred=y_pred))
-
-            # print(classification_report(y_true=y_train,
-            #                             y_pred=y_pred))
-            
-            # # Plot Optimal Tree
-            # feature_names = names
-            # class_names = ['Not-Fraud', 'Fraud']
-
-            # dot_data = tree.export_graphviz(selection_model,
-            #                                 out_file=None,
-            #                                 feature_names=feature_names,
-            #                                 class_names=class_names,
-            #                                 label='all',
-            #                                 impurity=True,
-            #                                 node_ids=True,
-            #                                 filled=True,
-            #                                 rounded=True,
-            #                                 leaves_parallel=True,
-            #                                 special_characters=False)
-
-            # graph = graphviz.Source(dot_data)
-            # graph.format = 'png'
-            # graph.render(filename='optimal_tree_fraud', directory='static/img/', view=False)
             return render_template('trueoutput.html')
 @app.route('/trueoutput', methods=["GET"])
 def true_output():
